bc8d228419f46410d22d1d8510cda448654a1865
Tom Sixma <sixmatom@gmail.com>
2019-09-18 15:52:07 +0200

    koe.py aangevuld

--- koe.py
+++ koe.py
@@ -10,7 +10,17 @@ class Feed:
         if koe.weight == 450:
             self.energy = 26500
             self.protein = 215
-        else:
+        elif koe.weight == 500:
             self.energy = 29500
             self.protein = 245
+        elif koe.weight == 575:
+            self.energy = 31500
+            self.protein = 255  
+        elif koe.weight == 600:
+            self.energy = 37000
+            self.protein = 305
+        else:
+            self.energy = 40000
+            self.protein = 350
+ 
 
