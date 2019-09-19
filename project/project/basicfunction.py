5e382751bbed74761cb74b177f960e462d998416
Tom Sixma <sixmatom@gmail.com>
2019-09-19 15:24:27 +0200

    map opgeruimd

--- project/project/basicfunction.py
+++
@@ -1,9 +0,0 @@
-class BasicFunction(object): 
-    def __init__(self): 
-        self.state = 0
- 
-    def increment_state(self): 
-        self.state += 1
- 
-    def clear_state(self): 
-        self.state = 0
